a219509a7a54de4bc80bc8158f1d33170f48fdcd
helpvolt <helpvolt@gmail.com>
2026-09-05 12:49:40 -0300

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
+import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
-@app.route("/")
+@app.route('/')
 def home():
-    # Agora ele vai procurar um arquivo chamado index.html na pasta templates
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return render_template('index.html')
